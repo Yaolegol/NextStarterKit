09580e3535d2192a9d59c3ebfa9895fa8d6f9be1
Oleg <m160160@yandex.ru>
2021-02-21 20:54:28 +0700

    add all pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fc9e1b03d0ea7f02c799";
+/******/ 	var hotCurrentHash = "f53e3c3af71c02ca5334";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
